e12f94a298068eb513afe5d105234e97a9b996ae
Ryan Swallow <rswallow2021@jpkeefehs.org>
2021-03-30 11:50:06 -0400

    SGHHHHHHHHHHHHHESH

--- Test/src/Main.java
+++ Test/src/Main.java
@@ -1,9 +1,50 @@
 
+//
+import java.io.*;
+import java.util.Arrays;
+
+/**
+ * Copy one file to another using low level byte streams,
+ * read and write a whole.at once.
+ * @author www.codejava.net
+ */
 public class Main {
-
-	public static void main(String[] args) {
-		System.out.println("pog");
-
-	}
-
-}
+ 
+    public static void main(String[] args) {
+ 
+        String inputFile = "C:\\Users\\rswal\\Downloads\\Tom_Jerry.pgm";
+        String outputFile = "output.xyz";
+ 
+ 
+        try (
+            InputStream inputStream = new FileInputStream(inputFile);
+            OutputStream outputStream = new FileOutputStream(outputFile);
+        ) {
+ 
+            long fileSize = new File(inputFile).length();
+ 
+            byte[] allBytes = new byte[(int) fileSize];
+ 
+            inputStream.read(allBytes);
+ 
+            outputStream.write(allBytes);
+            byte lastByte;
+            for(int i =0;i<allBytes.length;i++) 
+            {
+            	byte curByte = allBytes[i];
+            	if(i<16) 
+            	{
+              	  	
+            	}
+            	else 
+            	{
+            		
+            		
+            	}
+            	lastByte = curByte;
+            }
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+    }
